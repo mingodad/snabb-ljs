05688585cde6018e67d13bdab880e34dafdf24b5
mingodad <mingodad@gmail.com>
2018-12-21 10:44:47 +0100

    Fix remaining lua code

--- lib/pflua/common.mk
+++ lib/pflua/common.mk
@@ -1,5 +1,5 @@
 ABS_TOP_SRCDIR:=$(shell cd $(TOP_SRCDIR) && pwd)
-LUAJIT=$(ABS_TOP_SRCDIR)/deps/luajit/usr/local/bin/luajit
+LUAJIT=$(ABS_TOP_SRCDIR)/deps/luajit/usr/local/bin/ljsjit
 PATH := $(ABS_TOP_SRCDIR)/deps/luajit/usr/local/bin:$(PATH)
-export LUA_PATH := $(ABS_TOP_SRCDIR)/deps/dynasm/?.lua;;
+export LUA_PATH := $(ABS_TOP_SRCDIR)/deps/dynasm/?.ljs;;
 export LD_LIBRARY_PATH := $(ABS_TOP_SRCDIR)/deps/dynasm:$(LD_LIBARARY_PATH)
